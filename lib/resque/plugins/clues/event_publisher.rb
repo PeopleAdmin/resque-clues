c039bd77fd89b779d1d5aec75010c21da750d4dc
Lance Woodson <lance.woodson@peopleadmin.com>
2013-05-04 08:18:34 -0500

    Enqueued event for StandardOut event publisher
    Added tests and implementation of enqueued events for the
    StandardOut event publisher.  Also made pry available to tests
    for debugging purposes.

--- lib/resque/plugins/clues/event_publisher.rb
+++ lib/resque/plugins/clues/event_publisher.rb
@@ -1,7 +1,18 @@
+require 'pp'
+
 module Resque
   module Plugins
     module Clues
       module EventPublisher
+        EVENT_TYPES = %w[enqueued]
+
+        class StandardOut
+          EVENT_TYPES.each do |event_type|
+            define_method(event_type) do |queue, metadata, klass, *args|
+              puts({event_type: event_type, queue: queue, metadata: metadata, worker_class: klass, args: args})
+            end
+          end
+        end
       end
     end
   end
--- spec/event_publisher_spec.rb
+++ spec/event_publisher_spec.rb
@@ -1,7 +1,25 @@
 require 'spec_helper'
+require 'stringio'
+require 'json'
 
 describe Resque::Plugins::Clues::EventPublisher do
   it "should pass Resque lint detection" do
     Resque::Plugin.lint(Resque::Plugins::Clues::EventPublisher) 
+  end
+
+  describe Resque::Plugins::Clues::EventPublisher::StandardOut do
+    def verify_output(hash)
+      STDOUT.should_receive(:puts).with(hash)
+    end
+
+    before do
+      @publisher = Resque::Plugins::Clues::EventPublisher::StandardOut.new
+    end
+
+    it "should send enqueued event to STDOUT" do
+      verify_output :event_type=>"enqueued", :queue=>:test_queue, :metadata=>{}, 
+                    :worker_class=>"FooBar", :args=>["a", "b"]
+      @publisher.enqueued(:test_queue, {}, "FooBar", "a", "b")
+    end
   end
 end
