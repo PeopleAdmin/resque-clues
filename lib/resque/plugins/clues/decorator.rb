f0699f236b58ebe144357dde8121540aac1b1b06
lwoodson <lance@webmaneuvers.com>
2013-05-22 07:02:59 -0500

    Implementing QueueDecorator
    Added basic tests and implementation of QueueDecorator, ensuring that
    the Resque push/pop interface is preserved and old methods appropriately
    invoked.  Also a start on event hashing.

--- lib/resque/plugins/clues/decorator.rb
+++ lib/resque/plugins/clues/decorator.rb
@@ -1,7 +1,41 @@
+require 'digest/md5'
+
 module Resque
   module Plugins
     module Clues
-      module Decorator
+      module QueueDecorator
+        def push(queue, item)
+          item[:metadata] = {
+            event_hash: event_hash
+          }
+          _base_push(queue, item)
+        end
+
+        def pop(queue)
+          _base_pop(queue)
+        end
+
+        private
+        def event_hash
+          Digest::MD5.hexdigest("#{hostname}#{process}#{time}")
+        end
+
+        def hostname
+          `hostname`.chop
+        end
+
+        def process
+          $$
+        end
+
+        def time
+          Time.new.utc.to_f
+        end
+
+        def self.extended(klass)
+          alias_method :_base_push, :push
+          alias_method :_base_pop, :pop
+        end
       end
     end
   end
--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -1,7 +1,49 @@
 require 'spec_helper'
 
-describe Resque::Plugins::Clues::Decorator do
+describe Resque::Plugins::Clues::QueueDecorator do
   it "should pass Resque lint detection" do
-    Resque::Plugin.lint(Resque::Plugins::Clues::Decorator) 
+    Resque::Plugin.lint(Resque::Plugins::Clues::QueueDecorator) 
+  end
+
+  before do
+    Resque.send(:extend, Resque::Plugins::Clues::QueueDecorator)
+  end 
+
+  it "should expose original push method as _base_push" do
+    Resque.respond_to?(:_base_push).should == true
+  end
+
+  it "should expose original pop method as _base_pop" do
+    Resque.respond_to?(:_base_pop).should == true 
+  end
+
+  describe "#push" do
+    it "should invoke _base_push with a queue and item args and return the result" do 
+      Resque.stub(:_base_push) do |queue, item|
+        queue.should == :test_queue
+        item.nil?.should == false
+        "received"
+      end
+      Resque.push(:test_queue, {}).should == "received"
+    end
+
+    context "adds metadata to item stored in redis" do
+      it "should contain an event_hash identifying the job entering the queue" do
+        Resque.stub(:_base_push) do |queue, item|
+          item[:metadata][:event_hash].nil?.should == false
+        end
+        Resque.push(:test_queue, {})
+      end
+    end
+  end
+
+  describe "#pop" do
+    it "should invoke _base_pop with a queue arg and return the result" do
+      Resque.stub(:_base_pop) do |queue|
+        queue.should == :test_queue
+        "received"
+      end
+      Resque.pop(:test_queue).should == "received"
+    end
   end
 end
