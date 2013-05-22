1614dc4cb51beec221333232e9c039e6c14de07f
lwoodson <lance@webmaneuvers.com>
2013-05-22 05:28:03 -0500

    Rdocs for event publishers and whitespace cleanup

--- lib/resque/plugins/clues/event_publisher.rb
+++ lib/resque/plugins/clues/event_publisher.rb
@@ -3,9 +3,19 @@ require 'pp'
 module Resque
   module Plugins
     module Clues
+      # Contains event publisher classes that can be used to broadcast the
+      # following Resque job related events:
+      #
+      # enqueued:: When any job enters a queue
+      # dequeued:: When any job leaves a queue for execution
+      # destroyed:: When any job is discarded from a queue
+      # perform_started:: When any job is is about to be performed.
+      # perform_finished:: When any job has successfully finished performing
+      # failed:: When an exception was raised while performing a job.
       module EventPublisher
         EVENT_TYPES = %w[enqueued dequeued destroyed perform_started perform_finished failed]
 
+        # Event publisher base class with shared logic between all publishers.
         class Base
           private
           def build_hash(event_type, queue, metadata, worker_class, args)
@@ -19,6 +29,8 @@ module Resque
           end
         end
 
+        # Event publisher that publishes events to standard output in a json
+        # format.
         class StandardOut < Base
           EVENT_TYPES.each do |event_type|
             define_method(event_type) do |queue, metadata, klass, *args|
@@ -27,6 +39,8 @@ module Resque
           end
         end
 
+        # Event publisher that publishes events to a log file using ruby's
+        # stdlib logger and an optional formatter..
         class Log < Base
           attr_reader :logger
 
@@ -42,6 +56,9 @@ module Resque
           end
         end
 
+        # A composite event publisher that groups several child publishers so
+        # that events received are delegated to each of the children for
+        # further processing.
         class Composite < SimpleDelegator
           def initialize
             super([])
@@ -55,7 +72,7 @@ module Resque
             end
           end
 
-          private 
+          private
           def error(event_type, child)
             p "Error processing #{event_type} in #{child}"
           end
