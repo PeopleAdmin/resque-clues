c057457baf3aac4d9ec9040745f650bd9b2c77e1
lwoodson <lance@webmaneuvers.com>
2013-06-10 20:07:07 -0500

    Modify TestPublisher to support multiple events.

--- spec/test_publisher.rb
+++ spec/test_publisher.rb
@@ -1,11 +1,42 @@
 class TestPublisher
-  attr_reader :event_type, :timestamp, :queue, :metadata, :klass, :args
+  attr_reader :events
+
+  def initialize
+    @events = []
+  end
+
   def publish(event_type, timestamp, queue, metadata, klass, *args)
-    @event_type = event_type
-    @timestamp = timestamp
-    @queue = queue
-    @metadata = metadata
-    @klass = klass
-    @args = args
+    events << {
+      event_type: event_type,
+      timestamp: timestamp,
+      queue: queue,
+      metadata: metadata,
+      klass: klass,
+      args: args
+    }
+  end
+
+  def event_type(tail=-1)
+    @events[tail][:event_type]
+  end
+
+  def timestamp(tail=-1)
+    @events[tail][:timestamp]
+  end
+
+  def queue(tail=-1)
+    @events[tail][:queue]
+  end
+
+  def metadata(tail=-1)
+    @events[tail][:metadata]
+  end
+
+  def klass(tail=-1)
+    @events[tail][:klass]
+  end
+
+  def args(tail=-1)
+    @events[tail][:args]
   end
 end
