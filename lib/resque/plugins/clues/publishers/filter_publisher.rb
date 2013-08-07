1d8c3f6c218cc622f0ff46a6720ea6c3749d7946
Monte Morast <monte.morast@peopleadmin.com>
2013-08-06 22:50:27 -0500

    adding filter_publisher.rb

--- lib/resque/plugins/clues/publishers/filter_publisher.rb
+++ lib/resque/plugins/clues/publishers/filter_publisher.rb
@@ -1,22 +1,46 @@
-      # Event publisher that publishes events to a configurable indexers for datam-ining..
-      class IndexerPublisher
-        attr_reader :logger
 
-        # Creates a new LogPublisher that writes events to a log file at the
-        # specified log_path, using an optional formatter.  The default format
-        # will simply be the event in a json format, one per line.
-        #
-        # log_path:: The path to the log file.
-        # formatter:: A lambda formatter for log messages.  Defaults to writing
-        # one event per line.  See
-        # http://www.ruby-doc.org/stdlib-1.9.3/libdoc/logger/rdoc/Logger/Formatter.html
-        def initialize(log_path, formatter=nil)
-          @logger = Logger.new(log_path)
-          @logger.formatter = formatter || lambda {|severity, time, program, msg| msg}
+module Resque
+  module Plugins
+    module Clues
+      # Event publisher that monitors events and indexes them for data mining.
+      class FilterPublisher
+
+        attr_reader events
+
+        # Initializer for the IndexerPublisher class
+        # [Parameters]
+        #  * options = Hash containing containing optional configuration parameters. See parameters below
+        def initialize(options={})
+
+
         end
 
         # Publishes an event to the log.
         def publish(event_type, timestamp, queue, metadata, klass, *args)
+          event = event_hash(event_type, timestamp, queue, metadata, klass, args)
+
+
+          @events.push(event)
+
 
         end
+
+
+        private
+          def event_hash(event_type, timestamp, queue, metadata, klass, *args)
+            current_event = {:event_type => event_type,
+                             :timestamp => timestamp,
+                             :queue => queue,
+                             :metadata => metadata,
+                             :klass => klass,
+                             :args => args
+                             }
+          end
+
+
+
+
       end
+    end
+  end
+end
